d1d2977bae19b29555b7245f46fb73ce99d557bb
Jochen Brissier <j.brissier@me.com>
2015-05-15 12:26:26 +0200

    register cams

--- base/urls.py
+++ base/urls.py
@@ -5,15 +5,18 @@ from django.views.generic import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='home.html')),
+#    url(r'^$', TemplateView.as_view(template_name='home.html')),
 
 
     # login / logout
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', name="login"),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.login', name="logout"),
 
-
-
     # admin page
-    url(r'^admin/', include(admin.site.urls))
+    url(r'^admin/', include(admin.site.urls)),
+
+    # cms
+    url(r'^', include('cms.urls')),
+
+
 )
